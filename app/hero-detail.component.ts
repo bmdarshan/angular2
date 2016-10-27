c6dfc06ad5a9af7db2c39de531aed771a951176d
bmdarshan <darshan.bmg@gmail.com>
2016-10-27 16:59:26 -0400

    fix the hero-detail compilation issue

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,16 +1,13 @@
-// Keep the Input import for now, we'll remove it later:
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params }   from '@angular/router';
-import { Location }                 from '@angular/common';
-
-import { HeroService } from './hero.service';
-
+import { Component, Input } from '@angular/core';
+import { Hero } from './hero';
 @Component({
 	selector: 'my-hero-detail',
 	template: `
     <div *ngIf="hero">
       <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>id: </label>{{hero.id}}
+      </div>
       <div>
         <label>name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
@@ -19,7 +16,5 @@ import { HeroService } from './hero.service';
   `
 })
 export class HeroDetailComponent {
-
-	@Input()
-	hero: Hero;
+	@Input() hero: Hero;
 }
